f2abb014749a980f558b22c9d9711bf9e9e4467a
Ishan Bajaj <ishanb007@gmail.com>
2023-07-26 01:52:02 +0530

    Finishing up styling using Materials-UI

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import { Fab, Zoom } from "@mui/material";
 
 function CreateArea(props) {
 
@@ -6,6 +8,8 @@ function CreateArea(props) {
     title:"",
     content:""
   });
+  
+  const [noteBool, setNoteBool] = useState(false);
 
   function click(event){
     props.addNote(text);
@@ -14,6 +18,11 @@ function CreateArea(props) {
       content:""
     })
     event.preventDefault();
+  }
+
+  function setup(event){
+        event.target.rows="3";
+        setNoteBool((prev)=>!prev);
   }
 
   function update(event){
@@ -43,10 +52,11 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title"  value={text.title} onChange={update}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" value={text.content} onChange={update}/>
-        <button onClick={click}>Add</button>
+      <form className="create-note">
+        <input name="title" placeholder="Title"  value={text.title} onChange={update} hidden={!noteBool}/>
+        <textarea name="content" placeholder="Take a note..." rows={noteBool ? 3 : 1} value={text.content} onChange={update} onClick={setup}/>
+        {/* rows value has been changed using conditional rendering but it is also possible with accessing the target element using event.target. */}
+        <Zoom in={noteBool}><Fab onClick={click}><AddIcon /></Fab></Zoom>
       </form>
     </div>
   );
